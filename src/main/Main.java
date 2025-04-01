658d976259bfc2f515f5b4d53266596d65062a88
XDProjecta <gimosquera@uninorte.edu.co>
2025-04-01 10:24:29 -0500

    ambas

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,8 @@ package main;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("holaaa cachona");
+        System.out.println("hola cachón");
 
+        System.out.println("holaaa cachona");
     }
 }
